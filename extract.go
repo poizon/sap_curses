85281315bcf71adf0b951874c3d4d81cd240670f
Pavel <pkuptsov@gmail.com>
2017-04-12 15:47:20 +0300

    WIP: MSSQL begins

--- extract.go
+++ extract.go
@@ -11,6 +11,8 @@ import (
 	"regexp"
 	"strconv"
 	"time"
+	"database/sql"
+  _ "github.com/denisenkom/go-mssqldb"
 )
 
 // импорт курсов в SAP
@@ -43,10 +45,10 @@ func main() {
 	configuration, _ := readConfig()
 	// готовим карту для хранения значений вида КОД_ВАЛЮТЫ => Курс на дату
 	curses := make(map[string]float64)
-	// получаем текущую дату и форматим ее
-	now_date := formatDate()
+	// получаем дату
+	cur_time := time.Now()
 	// читаем с сервера данные и парсим в структуру parsed
-	resp, err := http.Get(configuration.Url + now_date)
+	resp, err := http.Get(configuration.Url + formatDate("web", cur_time))
 	logError(err)
 	var parsed Result
 	defer resp.Body.Close()
@@ -61,7 +63,7 @@ func main() {
 		// заменяем все , на .
 		re := regexp.MustCompile("(,{1})")
 		num := re.ReplaceAllString(value.Value, ".")
-		// конвертируем строку в число с плавающей точкой
+		// конвертируем строку в число с плавающей точкой float64
 		dig, err := strconv.ParseFloat(num, 64)
 		// если ок - сохряняем в карту
 		if err == nil {
@@ -70,8 +72,25 @@ func main() {
 
 	}
 
-	//Теперь нужно сохранить значения валют в БД, которые прописаны в конфиге
-	fmt.Printf("%v", curses["USD"])
+	// Теперь нужно сохранить значения валют во все БД, которые прописаны в конфиге
+	// Правильней делать конечно USE DB вместо полного реконнекта, но
+	// TODO: переделать чтобы можно было инсертить на разные сервера, а не в разные БД
+	for _, dbname := range configuration.Db {
+		dsn := "server=" + configuration.Server + ";user id=" + configuration.User + ";password=" + configuration.Pass + ";database=" + dbname
+		db, err := sql.Open("mssql", dsn)
+    logFatal(err)
+    // ping db test
+		err = db.Ping()
+    logFatal(err)
+
+		defer db.Close()
+
+		for _, value := range configuration.ValCode {
+			getSQL(formatDate("sql", cur_time), value, curses[value])
+		}
+
+	}
+
 }
 
 // логгер ошибок
@@ -107,9 +126,7 @@ func readConfig() (Configuration, error) {
 }
 
 // форматируем дату
-func formatDate() string {
-
-	t := time.Now()
+func formatDate(format string, t time.Time) string {
 
 	var year = fmt.Sprintf("%v", t.Year())
 	var month = fmt.Sprintf("%d", t.Month())
@@ -122,9 +139,35 @@ func formatDate() string {
 	if len(day) < 2 {
 		day = "0" + day
 	}
-
-	format := day + "/" + month + "/" + year
+	// если формат для sql
+	if format == "sql" {
+		return fmt.Sprintf("%s-%s-%s", year, day, month)
+		// или для web
+	} else if format == "web" {
+		return fmt.Sprintf("%s/%s/%s", day, month, year)
+	}
 
 	return format
 }
 
+// текущая дата для MSSQL, код валюты и значение курса
+// dsn := "server=" + *server + ";user id=" + *userid + ";password=" + *password + ";database=" + *database
+//   db, err := sql.Open("mssql", dsn)
+//   if err != nil {
+//     fmt.Println("Cannot connect: ", err.Error())
+//     return
+//   }
+//   err = db.Ping()
+//   if err != nil {
+//     fmt.Println("Cannot connect: ", err.Error())
+//     return
+//   }
+//   defer db.Close()
+func getSQL(cur_date string, charcode string, curs float64) {
+	// my $sql = qq(INSERT INTO ORTT VALUES('$sql_date',N'$key',$hash{$key},'I',13));
+	//db.Query("SELECT * FROM t WHERE a = ?3, b = ?2, c = ?1", "x", "y", "z")
+	fmt.Print("[ " + cur_date + "] ")
+	fmt.Print(charcode + " ")
+	fmt.Println(curs)
+	//return "INSERT INTO ORTT VALUES('" + cur_date + "', N'" + charcode + "','" + curs + "','I',13)
+}
